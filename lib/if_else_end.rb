10c5e321102f4f560c593980b2bc9c33fef8bfcb
Blaire Baker <bakerblaire@gmail.com>
2020-10-11 03:24:05 +0000

    Done.

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,1 +1,10 @@
 # Write your solution here
+
+current_time = Time.now
+current_time = current_time.to_i
+is_even_or_odd = current_time % 2
+if is_even_or_odd == 0
+  puts "Even!"
+else
+  puts "Odd!"
+end
